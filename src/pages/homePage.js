c70efb90125b9fdad8b7eeac1610ac617325d741
bhaumik55231 <bhaumik55231@gmail.com>
2019-06-19 13:21:10 -0400

    Minor change

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -4,10 +4,12 @@ export function template() {
         <img src="https://dceg.cancer.gov/PublishedContent/Images/research/cancer-types/breast-cancer/Confluence_Logo.__v10025474.png" height="250px" width="250px" alt="NCI Confluence Data Commons">
         </br></br></br>
         <div style="text-align:left">
-            About confluence: - <a href="https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project" target="_blank">https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project</a></br>
-            Source Code: - <a href="https://github.com/episphere/confluence" target="_blank">https://github.com/episphere/confluence</a></br>
-            GitHub issues: - <a href="https://github.com/episphere/confluence/issues" target="_blank">https://github.com/episphere/confluence/issues</a></br>
-            Please <strong>Login</strong> with your <strong>box account</strong>, to explore!
+            <ul>
+                <li>About confluence: - <a href="https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project" target="_blank">https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project</a></li>
+                <li>Source Code: - <a href="https://github.com/episphere/confluence" target="_blank">https://github.com/episphere/confluence</a></li>
+                <li>GitHub issues: - <a href="https://github.com/episphere/confluence/issues" target="_blank">https://github.com/episphere/confluence/issues</a></li>
+                <li>Please <strong>Login</strong> with your <strong>box account</strong>, to explore!</li>
+            </ul>
         </div>  
     `;
 }
