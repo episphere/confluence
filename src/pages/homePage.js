e3c8f59b4a3d3ffc520f7a6dba7ba1aea816cf51
bhaumik55231 <bhaumik55231@gmail.com>
2019-06-19 13:18:31 -0400

    home page updates

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -6,7 +6,8 @@ export function template() {
         <div style="text-align:left">
             About confluence: - <a href="https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project" target="_blank">https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project</a></br>
             Source Code: - <a href="https://github.com/episphere/confluence" target="_blank">https://github.com/episphere/confluence</a></br>
-            GitHub issues: - <a href="https://github.com/episphere/confluence/issues" target="_blank">https://github.com/episphere/confluence/issues</a>
+            GitHub issues: - <a href="https://github.com/episphere/confluence/issues" target="_blank">https://github.com/episphere/confluence/issues</a></br>
+            Please <strong>Login</strong> with your <strong>box account</strong>, to explore!
         </div>  
     `;
 }
