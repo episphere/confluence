1afff6931948064b16c2ea2918b1cfb7c7b9a4c7
bhaumik55231 <bhaumik55231@gmail.com>
2019-06-24 15:04:12 -0400

    Adding icons to navbar links

--- src/components/navBarMenuItems.js
+++ src/components/navBarMenuItems.js
@@ -2,11 +2,11 @@ const template = () => {
     return `
         <div class="grid-border"></div>
         <div class="nav-item grid-elements">
-            <a class="nav-link nav-menu-links" href="#" title="Data Platform Summary" id="dataSummary">Data Exploration</a>
+            <a class="nav-link nav-menu-links" href="#" title="Data Platform Summary" id="dataSummary"><i class="fas fa-chart-pie"></i> Data Exploration</a>
         </div>
         <div class="grid-border"></div>
         <div class="nav-item grid-elements">
-            <a class="nav-link nav-menu-links" href="#" title="Data Submission"  id="dataSubmission">Data Submission</a>
+            <a class="nav-link nav-menu-links" href="#" title="Data Submission"  id="dataSubmission"><i class="fas fa-upload"></i> Data Submission</a>
         </div>
         <div class="grid-border"></div>
     `;
