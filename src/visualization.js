f46217c23a0caed8c6ab30c704d18186d6d96358
bhaumik55231 <bhaumik55231@gmail.com>
2019-12-16 14:30:39 -0500

    updating color scheme

--- src/visualization.js
+++ src/visualization.js
@@ -71,13 +71,13 @@ export const getFileContent = async (allIds) => {
     dc.config.defaultColors(d3.schemePaired);
     renderPieChart(cf, jsonData, 'consortium', 'dataSummaryVizChart7', 'dataSummaryVizLabel7', 'selectedRange7', 'chartDiv7');
     generateBarSingleSelect(cf, 'famHist', 'dataSummaryVizChart6', 'dataSummaryVizLabel6', 'selectedRange6', 'chartDiv6')
-    dc.config.defaultColors(d3.schemePaired);
+    dc.config.defaultColors(d3.schemePastel1);
     renderPieChart(cf, jsonData, 'study', 'dataSummaryVizChart1', 'dataSummaryVizLabel1', 'selectedRange1', 'chartDiv1');
     dc.config.defaultColors(d3.schemeSet2);
     renderPieChart(cf, jsonData, 'status', 'dataSummaryVizChart2', 'dataSummaryVizLabel2', 'selectedRange2', 'chartDiv2');
     dc.config.defaultColors(d3.schemeBrBG[8]);
     renderPieChart(cf, jsonData, 'ER_statusIndex', 'dataSummaryVizChart4', 'dataSummaryVizLabel4', 'selectedRange4', 'chartDiv4');
-    dc.config.defaultColors(d3.schemePastel1);
+    dc.config.defaultColors(d3.schemeSet3);
     renderPieChart(cf, jsonData, 'ethnicityClass', 'dataSummaryVizChart5', 'dataSummaryVizLabel5', 'selectedRange5', 'chartDiv5');
     // document.getElementById('chartInteractionMsg').innerHTML = `<i class="fas fa-info-circle"></i> Click on chart(s) for interaction!`;
     // setTimeout(() => document.getElementById('chartInteractionMsg').innerHTML = ``, 5000);
