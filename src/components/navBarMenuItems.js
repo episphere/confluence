75f896dae23b5fd1964c0678ebb6949a230b2eaf
bhaumik55231 <bhaumik55231@gmail.com>
2020-03-17 15:20:30 -0400

    updating navbar

--- src/components/navBarMenuItems.js
+++ src/components/navBarMenuItems.js
@@ -5,14 +5,18 @@ export const template = () => {
             <a class="nav-link nav-menu-links dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-database"></i> Data</a>
             <div class="dropdown-menu navbar-dropdown" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item nav-link nav-menu-links" href="#data_exploration" title="Data Exploration" id="dataSummary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-chart-bar"></i> Data Exploration
+                    <i class="fas fa-chart-bar"></i> Explore
                 </a>
                 <a class="dropdown-item nav-link nav-menu-links" href="#data_submission" title="Data Submission" id="dataSubmission"> 
-                    <i class="fas fa-upload"></i> Data Submission
+                    <i class="fas fa-upload"></i> Submit
                 </a>
                 <div id="myProjectsNav"></div>
-                <a class="dropdown-item nav-link nav-menu-links" href="#data_analysis" title="Data Analysis" id="dataAnalysis"><i class="fas fa-database"></i> Data Analysis</a>
-                <a class="dropdown-item nav-link nav-menu-links" href="#data_request" title="Data Request" id="dataRequest"><i class="fas fa-database"></i> Data Request</a>
+                <a class="dropdown-item nav-link nav-menu-links" href="#data_analysis" title="Data Analysis" id="dataAnalysis">
+                    <i class="fas fa-database"></i> Analyze
+                </a>
+                <a class="dropdown-item nav-link nav-menu-links" href="#data_request" title="Data Request" id="dataRequest">
+                    <i class="fas fa-database"></i> Request
+                </a>
             </div>
         </div>
         <div id="governanceNav"></div>
