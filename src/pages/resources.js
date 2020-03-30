efa0cf6e1eff8da18875eb2533d1e40bd3dbcbe0
bhaumik55231 <bhaumik55231@gmail.com>
2020-03-30 15:19:05 -0400

    minor update

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -6,7 +6,7 @@ export const confluenceResources = () => {
                 <ul>
                     <li><a href="http://bcac.ccge.medschl.cam.ac.uk/" target="_blank">BCAC - The Breast Cancer Association Consortium</a></li>
                     <li><a href="http://cimba.ccge.medschl.cam.ac.uk/" target="_blank">CIMBA - Consortium of Investigators of Modifiers of BRCA1/2</a></li>
-                    <li><a href="http://bcac.ccge.medschl.cam.ac.uk/" target="_blank">LAGENO - The Latin America Genomics Breast Cancer Consortium</a></li>
+                    <li>LAGENO - The Latin America Genomics Breast Cancer Consortium</li>
                     <li><a href="http://enigmaconsortium.org/" target="_blank">ENIGMA - Evidence-based Network for the Interpretation of Germline Mutant Alleles</a></li>
                 </ul>
                 <span class="confluence-pink">Institutions of Interest:</span></br>
