6330c2ac8d60ad3a2f070fe0bfeb237ca5258817
bhaumik55231 <bhaumik55231@gmail.com>
2020-06-09 11:14:06 -0400

    updating footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,30 @@
 export const footerTemplate = () => {
     return `
-        <div class="row footer-content">
-            <div class="col content-wrapper">
-                <ul class="menu">
-                    <li class="menu-item links-border-right"><a class="footer-links" href="https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project">DCEG Confluence</a></li>
-                    <li class="menu-item links-border-right"><a class="footer-links" href="https://dceg.cancer.gov/">DCEG Home</a></li>
-                    <li class="menu-item links-border-right"><a class="footer-links" href="https://dceg.cancer.gov/about/contact-dceg">Contact DCEG</a></li>
-                    <li class="menu-item links-border-right"><a class="footer-links" href="https://www.cancer.gov/global/web/policies">Policies</a></li>
-                    <li class="menu-item links-border-right"><a class="footer-links" href="https://www.cancer.gov/global/web/policies/accessibility">Accessibility</a></li>
-                    <li class="menu-item links-border-right"><a class="footer-links" href="https://www.cancer.gov/global/viewing-files">Viewing files</a></li>
-                    <li class="menu-item links-border-right"><a class="footer-links" href="https://www.cancer.gov/global/web/policies/foia">FOIA</a></li>
-                    <li class="menu-item"><a class="footer-links" href="https://dceg.cancer.gov/about/sitemap">DCEG Sitemap</a></li>
-                </ul>
-                <ul class="menu">
-                    <li class="menu-item links-border-right"><a class="footer-links" href="http://www.hhs.gov/">U.S. Department of Health and Human Services</a></li>
-                    <li class="menu-item links-border-right"><a class="footer-links" href="http://www.nih.gov/">National Institutes of Health</a></li> 
-                    <li class="menu-item links-border-right"><a class="footer-links" href="http://www.cancer.gov/">National Cancer Institute</a></li> 
-                    <li class="menu-item"><a class="footer-links" href="http://www.usa.gov/">USA.gov</a></li>
-                </ul>
-                <p class="menu footer-tagline">NIH…Turning Discovery Into Health<sup>®</sup></p>
+        <div class="footer-content">
+            <div class="row footer-row">
+                <div class="col-auto footer-col">
+                    <div class="menu-item links-border-right"><a class="footer-links" href="https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project">DCEG Confluence</a></div>
+                    <div class="menu-item links-border-right"><a class="footer-links" href="https://dceg.cancer.gov/">DCEG Home</a></div>
+                    <div class="menu-item links-border-right"><a class="footer-links" href="https://dceg.cancer.gov/about/contact-dceg">Contact DCEG</a></div>
+                    <div class="menu-item links-border-right"><a class="footer-links" href="https://www.cancer.gov/global/web/policies">Policies</a></div>
+                    <div class="menu-item links-border-right"><a class="footer-links" href="https://www.cancer.gov/global/web/policies/accessibility">Accessibility</a></div>
+                    <div class="menu-item links-border-right"><a class="footer-links" href="https://www.cancer.gov/global/viewing-files">Viewing files</a></div>
+                    <div class="menu-item links-border-right"><a class="footer-links" href="https://www.cancer.gov/global/web/policies/foia">FOIA</a></div>
+                    <div class="menu-item"><a class="footer-links" href="https://dceg.cancer.gov/about/sitemap">DCEG Sitemap</a></div>
+                </div>
+            </div>
+            <div class="row footer-row">
+                <div class="col-auto footer-col">
+                    <div class="menu-item links-border-right"><a class="footer-links" href="http://www.hhs.gov/">U.S. Department of Health and Human Services</a></div>
+                    <div class="menu-item links-border-right"><a class="footer-links" href="http://www.nih.gov/">National Institutes of Health</a></div> 
+                    <div class="menu-item links-border-right"><a class="footer-links" href="http://www.cancer.gov/">National Cancer Institute</a></div> 
+                    <div class="menu-item"><a class="footer-links" href="http://www.usa.gov/">USA.gov</a></div>
+                </div>
+            </div>
+            <div class="row footer-row">
+                <div class="col-auto footer-col">
+                    <div class="menu-item footer-tagline">NIH…Turning Discovery Into Health<sup>®</sup></div>
+                </div>
             </div>
         </div>
         `;
