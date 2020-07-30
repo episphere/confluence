731ecf1b7ff8f0a58ab94b683f048fc27bb30f67
bhaumik55231 <bhaumik55231@gmail.com>
2020-07-30 13:23:08 -0400

    minor update

--- src/event.js
+++ src/event.js
@@ -1495,6 +1495,7 @@ const addEventUpdateSummaryStatsForm = () => {
         await uploadFileVersion(publicDataObj, publicDataFileId, 'application/json');
         form.querySelectorAll('[type="submit"]')[0].classList.remove('disabled');
         await getFile(summaryStatsFileId);
+        await getFileInfo(summaryStatsFileId);
         form.querySelectorAll('[type="submit"]')[0].innerHTML = 'Update data';
         removeActiveClass('update-summary-stats-btn', 'active-filter');
         let template = notificationTemplate(top, `<span class="successMsg">Summary statistics updated</span>`, `Summary statistics successfully updated, please reload to see updated data.`);
