0a8d7aaf8c89a488828a2a38fb7a52c9896fd26b
bhaumik55231 <bhaumik55231@gmail.com>
2021-03-30 11:34:20 -0400

    Adding data request page content

--- src/pages/dataRequest.js
+++ src/pages/dataRequest.js
@@ -8,7 +8,23 @@ export const template = () => {
                     </div>
                 </div>
                 <div class="data-submission div-border" style="padding-left: 1rem;">
-                    <h1><i class="fas fa-laptop-code"></i> Coming Soon...</h1>
+                    <div class="row m-0">
+                        The Confluence Project is currently generating genotyping data and harmonizing risk factor and clinical data. Data is expected to be available for request in late 2022.Data access will be facilitated through this platform, in accordance to the data use agreements signed between participating studies (originator) and Data Coordinating Centers from Consortia participating in Confluence.
+                    </div></br>
+                    <div class="row m-0 align-center">
+                        <img src="./static/images/data_request.PNG" alt="Overview of data sharing and access in Confluence" style="margin:auto">
+                    </div></br>
+                    <div class="row m-0">
+                        The following data access procedures are planned:
+                    </div>
+                    <div class="row m-0">
+                        <ul>
+                            <li>Researcher submits a study concept describing the project, including variables of interest, via the Confluence Data Platform. This request will be sent via the platform to the relevant consortia data access coordinating committees (DACCs) that govern the requested data.</li>
+                            <li>After approval by the relevant consortia DACCs, individual studies contributing data are notified and given a time period to opt-out their study from the approved project.</li>
+                            <li>After the opt-out period has elapsed, the researcher’s institution signs a data transfer agreement (DTA) for the study conceptwith each of the relevant consortium data coordinating center(s) governing the data.</li>
+                            <li>Upon DTA signatures, the data coordinating center(s) will be able to provide access of the approved data to researchers through the Confluence Data Platform.</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
