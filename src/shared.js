f6b84d00dd2393f374267fb475722a51f43be840
bhaumik55231 <bhaumik55231@gmail.com>
2021-10-01 22:29:10 -0400

    minor update

--- src/shared.js
+++ src/shared.js
@@ -163,7 +163,7 @@ export const storeAccessToken = async () => {
         }else if(location.origin.indexOf('episphere') !== -1){
             clt = config.iniAppProd
         }else if(location.origin.indexOf('confluence-stage.cancer.gov') !== -1){
-            clt = config.iniObs
+            clt = config.iniStage
         }
         document.getElementById('confluenceDiv').innerHTML = '';
         let url = `https://api.box.com/oauth2/token/`;
@@ -203,7 +203,7 @@ export const refreshToken = async () => {
     }else if(location.origin.indexOf('episphere') !== -1){
         clt = config.iniAppProd
     }else if(location.origin.indexOf('confluence-stage.cancer.gov') !== -1){
-        clt = config.iniObs
+        clt = config.iniStage
     }
 
     const response = await fetch(`https://api.box.com/oauth2/token/`, {
