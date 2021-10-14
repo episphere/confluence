8911199c5c1c1ce9ea5b58021a77739f611f02e5
Bhaumik Patel <bhaumik55231@gmail.com>
2021-10-14 15:55:53 -0400

    Improvements

--- src/shared.js
+++ src/shared.js
@@ -197,13 +197,10 @@ export const refreshToken = async () => {
     if(!localStorage.parms) return;
     const parms = JSON.parse(localStorage.parms);
     let clt={}
-    if(location.origin.indexOf('localhost') !== -1){
-        clt = config.iniAppLocal;
-    }else if(location.origin.indexOf('episphere') !== -1){
-        clt = config.iniAppDev
-    }else if(location.origin.indexOf('confluence-stage.cancer.gov') !== -1){
-        clt = config.iniAppStage
-    }
+    if(location.origin.indexOf('localhost') !== -1) clt = config.iniAppLocal;
+    else if(location.origin.indexOf('episphere') !== -1) clt = config.iniAppDev
+    else if(location.origin.indexOf(applicationURLs.stage) !== -1) clt = config.iniAppStage
+    else if(location.origin.indexOf(applicationURLs.prod) !== -1) clt = config.iniAppProd;
 
     const response = await fetch(`https://api.box.com/oauth2/token/`, {
         headers: {
