6b814260369942fab205c8d84bd94f41b5e25292
SrijonMukherjee <srijon.muk@gmail.com>
2022-02-22 10:09:26 -0500

    Edits to text in Participating Consortium page

--- src/pages/join.js
+++ src/pages/join.js
@@ -10,24 +10,24 @@ export const confluenceResources = () => {
                 <div class="main-summary-row white-bg div-border">
                     <div class="col font-size-18 align-left">
                         </br>
-                        <div>Participate in Confluence by joining one of the following breast cancer consortia:</div>
+                        <div>The following breast cancer consortia are participating in Confluence:</div>
                         <div class="consortia-desc">
                             <a href="http://bcac.ccge.medschl.cam.ac.uk/" target="__blank">Breast Cancer Association Consortium</a> (BCAC)</br>
                             The BCAC is a forum of investigators interested in the inherited risk of breast cancer. The aim of the consortium is to combine data from many studies, and to provide a reliable assessment of the risks associated with these gene.
                         </div>
 
                         <div class="consortia-desc">
-                            <a href="https://projectreporter.nih.gov/project_info_description.cfm?aid=9040408&icde=30184576&ddparam=&ddvalue=&ddsub=&cr=1&csb=default&cs=ASC" target="__blank">African Ancestry Breast Cancer Genetic Study</a> (AABCGS)</br>
-                            The AABCGS is a GWAS of African-American breast cancer cases and similar number of controls.
+                            <a href="https://projectreporter.nih.gov/project_info_description.cfm?aid=9040408&icde=30184576&ddparam=&ddvalue=&ddsub=&cr=1&csb=default&cs=ASC" target="__blank">African-ancestry Breast Cancer Genetic Consortium</a> (AABCG)</br>
+                            The AABCG is a collaboration of investigators focused on identifying novel genetic susceptibility factors for breast cancer in African-ancestry women and evaluating the influence of germline risk variants on breast cancer biology.
                         </div>
 
                         <div class="consortia-desc">
                             <a href="mailto:lfejerman@ucdavis.edu">Latin America Genomics Breast Cancer Consortium</a> (LAGENO-BC)</br>
-                            The LAGENO is a forum of investigators interested in the inherited risk of breast cancer. The aim of the consortium is to combine data from many studies, and to provide a reliable assessment of the risks associated with these gene.
+                            LAGENO-BC is a collaborative effort including a multinational team of investigators sharing knowledge and materials to facilitate breast cancer genetics research relevant to the diverse populations of Latin America and its diaspora.
                         </div>
 
                         <div class="consortia-desc">
-                            <a href="mailto:wei.zheng@vanderbilt.edu">Asia Breast Cancer Consortium</a> (ABCC)</br>
+                            <a href="mailto:wei.zheng@vanderbilt.edu">Asia Breast Cancer Consortium</a> </br>
                             The ABCC is a GWAS of Asian breast cancer cases and similar number of controls.
                         </div>
 
@@ -37,16 +37,15 @@ export const confluenceResources = () => {
                         </div>
 
                         <div class="consortia-desc">
-                            <a href="mailto:nick.orr@qub.ac.uk">Male Breast Cancer Genetics Consortium (MERGE)</a></br>
+                            <a href="mailto:nick.orr@qub.ac.uk">Male Breast Cancer Genetics Consortium</a></br>
                             The MERGE is a GWAS of male breast cancer cases and similar number of controls.
                         </div>
 
                         <div class="consortia-desc">
-                            Studies not part of a breast cancer consortia are participating through a direct collaboration with NCI Division of Cancer Epidemiology and Genetics
+                            Studies not part of a breast cancer consortia are participating through a direct collaboration with the National Cancer Institute, Division of Cancer Epidemiology and Genetics.
                         </div>
                         <div class="consortia-desc">
                             For more information:</br>
-                            Visit: <a href="https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project" target="__blank">https://dceg.cancer.gov/research/cancer-types/breast-cancer/confluence-project</a></br>
                             Email: <a href="mailto:ConfluenceProject@nih.gov">ConfluenceProject@nih.gov</a>
                         </div>
                     <div>
