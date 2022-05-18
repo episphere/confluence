a07c10f5047d96e7ff7f32ee4bf681e88470d217
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2022-05-18 16:21:22 -0400

    update login

--- src/manageAuthentication.js
+++ src/manageAuthentication.js
@@ -29,11 +29,11 @@ export const loginObs = () => {
 }
 
 export const loginAppDev = () => {
-    location.href=`https://account.box.com/api/oauth2/authorize?response_type=code&client_id=${config.iniAppLocal.client_id}&redirect_uri=${location.origin+location.pathname}?state=${config.iniAppLocal.stateIni}`
+    location.href=`https://account.box.com/api/oauth2/authorize?response_type=code&client_id=${config.iniAppLocal.client_id}&redirect_uri=${location.origin+location.pathname}&state=${config.iniAppLocal.stateIni}`
 }
 
 export const loginAppEpisphere = () => {
-    location.href=`https://account.box.com/api/oauth2/authorize?response_type=code&client_id=${config.iniAppDev.client_id}&redirect_uri=${applicationURLs.dev}&state=${config.iniAppDev.stateIni}`
+    location.href=`https://account.box.com/api/oauth2/authorize?response_type=code&client_id=${config.iniAppDev.client_id}&redirect_uri=${location.origin+location.pathname}&state=${config.iniAppDev.stateIni}`
 }
 
 export const loginAppProd = () => {
