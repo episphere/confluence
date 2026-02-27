362cb496325b0a357940994f41f517fda6ff40ed
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2026-02-27 08:48:39 -0500

    Updated dataRequest page

--- src/pages/dataRequest.js
+++ src/pages/dataRequest.js
@@ -1,27 +1,4 @@
 export const template = () => {
-    // return `
-    //     <div class="general-bg padding-bottom-1rem">
-    //         <div class="container body-min-height">
-    //             <div class="main-summary-row">
-    //                 <div class="align-left">
-    //                     <h1 class="page-header">Data Access Process</h1>
-    //                 </div>
-    //             </div>
-    //             <div class="data-submission div-border font-size-18" style="padding-left: 1rem;">
-    //                 <div class="row m-0">
-    //                     <ul>
-    //                         <li> Step 1: Researcher explores summary data to evaluate feasibility of proposed studies. </li>
-    //                         <li> Step 2: Researcher submits a study concept describing their project. The concept is sent to the relevant consortia <a href="#join/overview" target="_blank" rel="noopener noreferrer"> data access coordinating committees (DACCs)</a> for their review. </li>
-    //                         <li> Step 3: After a concept is approved by the relevant DACCs, the individual studies contributing the requested data are notified and given the option to participate in the approved project. </li>
-    //                         <li> Step 4: Researcher signs a data transfer agreement (DTA) with each of the relevant consortium data coordinating centers (DCCs) governing the requested data. </li>
-    //                         <li> Step 5: Researcher receives access to the request data from the DCCs. </li>
-    //                     </ul>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // `;
-
     return `
         <div class="general-bg padding-bottom-1rem">
             <div class="container body-min-height">
@@ -32,15 +9,14 @@ export const template = () => {
                 </div>
                 <div class="data-submission div-border font-size-18 black-font" style="padding-left: 1rem;">
                     <div class="mb-3">
-                        The Confluence Project is currently generating genotyping data and harmonizing risk factor and clinical data. This page will be updated when data is available for request by the research community. Data access will be in accordance to data use agreements signed between participating studies and the Data Coordinating Centers from Consortia participating in Confluence.
-                    </div>
+                        The Confluence Project is currently open to collaborators directly participating in the Confluence Project to submit proposals to request access to data shared with the project.  Researchers outside of the Confluence Project will be able to submit study proposals around the time of publication of the first paper. Data access will be in accordance to data use agreements signed between participating studies and the Data Coordinating Centers from consortia participating in Confluence.                    </div>
                     <div class="row m-0">The following data access procedures will be facilitated through this platform:</div>
                     <div class="row px-5">
                         <ul>
                             <li> Researcher submits a study concept describing the project, including variables of interest, via the Confluence Data Platform. This request will be sent to the relevant consortia data access coordinating committees (DACCs) that govern the requested data.</li>
                             <li> After approval by the relevant consortia DACCs, individual studies contributing data will be notified and given a time period to determine if their study will participate in the approved project. </li>
                             <li> After this period has elapsed, the researcher's institution will sign a data transfer agreement (DTA) for the study concept with each of the relevant consortium data coordinating center(s) governing the data. </li>
-                            <li> Upon DTA signatures, the  data coordinating center(s) will be able to provide access to the approved data.</li>
+                            <li> Upon DTA signatures, the data coordinating center(s) will be able to provide access to the approved data.</li>
                         </ul>
                     </div>
                     <div class="row m-0">The participating DACCs will review submitted concepts three times a year, according to the following 4-month submission windows:</div>
@@ -51,7 +27,8 @@ export const template = () => {
                             <li> March 1 - June 30</li>
                         </ul>
                     </div>
-                    <div class="row m-0">Currently, only Confluence collaborators can submit proposals to request access to Confluence Project data. Researchers outside of the Confluence Project will be able to submit study proposals around the time of publication of the first paper.</div>
+                    <div class="row m-0">Access to individual level data will be provided through a secure, cloud-based, trusted, research environment (TRE). Data will not be permitted to be downloaded, streamed, or otherwise, removed from the TRE. Researchers that are approved to access data on the TRE are responsible for paying the compute and data storage costs associated with their approved concept.</div>
+                    <div class="m-0">Analyses of Confluence Project data that are <b>stratified by study</b> must suppress exact counts in cells with fewer than 10 observations (<10). These counts should be represented as "<10." Additionally, any statistical test statistics derived from cells with <10 observations must be presented in a way that does not allow back-calculation of the exact number of individuals in those cells. </div>
                 </div>
             </div>
         </div>
@@ -69,15 +46,14 @@ export const templateAfterLogin = () => {
                 </div>
                 <div class="data-submission div-border font-size-18 black-font" style="padding-left: 1rem;">
                     <div class="mb-3">
-                        The Confluence Project is currently generating genotyping data and harmonizing risk factor and clinical data. This page will be updated when data is available for request by the research community. Data access will be in accordance to data use agreements signed between participating studies and the Data Coordinating Centers from Consortia participating in Confluence.
-                    </div>
+                        The Confluence Project is currently open to collaborators directly participating in the Confluence Project to submit proposals to request access to data shared with the project.  Researchers outside of the Confluence Project will be able to submit study proposals around the time of publication of the first paper. Data access will be in accordance to data use agreements signed between participating studies and the Data Coordinating Centers from consortia participating in Confluence.                    </div>
                     <div class="row m-0">The following data access procedures will be facilitated through this platform:</div>
                     <div class="row px-5">
                         <ul>
                             <li> Researcher submits a study concept describing the project, including variables of interest, via the Confluence Data Platform. This request will be sent to the relevant consortia data access coordinating committees (DACCs) that govern the requested data.</li>
                             <li> After approval by the relevant consortia DACCs, individual studies contributing data will be notified and given a time period to determine if their study will participate in the approved project. </li>
                             <li> After this period has elapsed, the researcher's institution will sign a data transfer agreement (DTA) for the study concept with each of the relevant consortium data coordinating center(s) governing the data. </li>
-                            <li> Upon DTA signatures, the  data coordinating center(s) will be able to provide access to the approved data.</li>
+                            <li> Upon DTA signatures, the data coordinating center(s) will be able to provide access to the approved data.</li>
                         </ul>
                     </div>
                     <div class="row m-0">The participating DACCs will review submitted concepts three times a year, according to the following 4-month submission windows:</div>
@@ -88,7 +64,8 @@ export const templateAfterLogin = () => {
                             <li> March 1 - June 30</li>
                         </ul>
                     </div>
-                    <div class="row m-0">Currently, only Confluence collaborators can submit proposals to request access to Confluence Project data. Researchers outside of the Confluence Project will be able to submit study proposals around the time of publication of the first paper.</div>
+                    <div class="row m-0">Access to individual level data will be provided through a secure, cloud-based, trusted, research environment (TRE). Data will not be permitted to be downloaded, streamed, or otherwise, removed from the TRE. Researchers that are approved to access data on the TRE are responsible for paying the compute and data storage costs associated with their approved concept.</div>
+                    <div class="m-0">Analyses of Confluence Project data that are <b>stratified by study</b> must suppress exact counts in cells with fewer than 10 observations (<10). These counts should be represented as "<10." Additionally, any statistical test statistics derived from cells with <10 observations must be presented in a way that does not allow back-calculation of the exact number of individuals in those cells. </div>
                 </div>
             </div>
         </div>
