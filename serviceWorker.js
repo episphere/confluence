538d4d3a646f6e6bdb7355d4133f4c87ce0e39b8
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2026-03-17 12:46:28 -0400

    Updated version

--- serviceWorker.js
+++ serviceWorker.js
@@ -38,7 +38,7 @@ registerRoute(
 );
 
 workbox.precaching.precacheAndRoute([
-        {url: 'index.html', revision: 'v2.3.5'},
-        {url: 'confluence.js', revision:'v2.3.5'}
+        {url: 'index.html', revision: 'v2.3.6'},
+        {url: 'confluence.js', revision:'v2.3.6'}
     ]
 );
